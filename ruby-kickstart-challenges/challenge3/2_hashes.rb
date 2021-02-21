d2f46eed83425a6d0f2d5da9218b90fff26f646b
EC2 Default User <tarandeep-nandhra@hotmail.co.uk>
2021-02-21 22:42:59 +0000

    Hashes and blocks

---
+++ ruby-kickstart-challenges/challenge3/2_hashes.rb
@@ -0,0 +1,22 @@
+# Given a nonnegative integer, return a hash whose keys are all the odd nonnegative integers up to it
+# and each key's value is an array containing all the even non negative integers up to it.
+#
+# Examples:
+# staircase 1  # => {1 => []}
+# staircase 2  # => {1 => []}
+# staircase 3  # => {1 => [], 3 => [2]}
+# staircase 4  # => {1 => [], 3 => [2]}
+# staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
+
+def staircase(n)
+  hash = Hash.new([])
+  even_numbers = []
+  for i in 1..n do
+    if i % 2 == 1
+      hash[i] = [*even_numbers]
+    else
+      even_numbers << i
+    end
+  end
+  hash
+end
